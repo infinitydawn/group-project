886a0049e286f39b56e881f7ad1f3f975b7dae22
Mike Khadeida <Mike.khadeida@gmail.com>
2024-08-01 15:51:16 -0400

    edited Create Account

--- Scripts/index.js
+++ Scripts/index.js
@@ -8,6 +8,22 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log(endpoint);
         getData(endpoint);
     })
+
+
+    const loginForm=document.querySelector("#login");
+    const createAccount=document.querySelector("#createAccount"); /*from id from login.html*/
+
+    document.querySelector("#linkCreateAccount"),addEventListener("click", () =>{
+        loginForm.classList.add("form-hidden");
+        createAccountform.classList.remove("form-hidden");
+
+    })
+    document.querySelector("#linkLogin"),addEventListener("click", () =>{
+        loginForm.classList.remove("form-hidden");
+        createAccountform.classList.add("form-hidden");
+        
+    })
+
 });
 
 
