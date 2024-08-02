11ff441b138d64eda6e4bb699f897e114a8651fd
Mikhail Nikulin <nklnmish@gmail.com>
2024-08-02 14:46:23 -0400

    gitignore test 5

--- Backend/databaseLink.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    dbKey : "mongodb+srv://nklnmish:mongoMN2024!@cluster0.1inkboy.mongodb.net/users?retryWrites=true&w=majority&appName=Cluster0"
-}
-
-// export default function getDatabaseKey(){
-//     return "mongodb+srv://nklnmish:mongoMN2024!@cluster0.1inkboy.mongodb.net/users?retryWrites=true&w=majority&appName=Cluster0"
-// }
